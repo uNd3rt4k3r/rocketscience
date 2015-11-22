1624f7b06298fe1081aa0f63797fdd49bbd926a6
uNd3rt4k3r <emil.dohlen.hansen@gmail.com>
2015-11-22 22:04:23 +0100

    Added functionality to find me button

--- assets/js/maphandling.js
+++ assets/js/maphandling.js
@@ -1,16 +1,20 @@
-/**
- * Created by Kristian on 21.11.2015.
- */
-
-function get_location() {
+function findMe() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(location_found);
+        navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        //window.alert("Browser doesn't support Geolocation");
+        alert("Well this is awkward, someone did screw up here");
     }
 }
 
-//Updates textboxes with position data
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    var myLatlng = new google.maps.LatLng(lat, lng);
+    setLocation(myLatlng);
+    draw_map(myLatlng);
+}
+
+/* UPDATES INPUTBOXES WITH POSITION DATA */
 function setLocation(position) {
     var lat = position.lat();
     var lon = position.lng();
@@ -25,7 +29,10 @@ var map;
 function initialize_map() {
     //Test data
     var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+    draw_map(myLatlng);
+}
 
+function draw_map(myLatlng) {
     var mapOptions = {
         center: myLatlng,
         scrollwheel: false,
@@ -35,7 +42,7 @@ function initialize_map() {
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     var marker = new google.maps.Marker({
-        position: {lat: -34.397, lng: 150.644},
+        position: myLatlng,
         map: map,
     });
 
@@ -45,56 +52,27 @@ function initialize_map() {
         marker = new google.maps.Marker({position: event.latLng, map: map});
         setLocation(event.latLng);
     });
-    // Try HTML5 geolocation
-    /*
-     if (navigator.geolocation) {
-     navigator.geolocation.getCurrentPosition(function(position) {
-     var pos = new google.maps.LatLng(position.coords.latitude,
-     position.coords.longitude);
-     map.setCenter(pos);
-     }, function() {
-     handleNoGeolocation(true);
-     });
-     get_location();
-     } else {
-     window.alert("Browser doesn't support Geolocation");
-     }
-     */
+
 }
 
-
-
-//var url = "http://192.168.0.196:8082/api/organisationUnits?format=json";
-function getData(){
-    //  var webApiURL = "http://cors.io/?u=https://apps.dhis2.org/demo/api/organisationUnits?format=json";
-    // var webApiURL = "http://Play.dhis2.org/api/organisationUnits?format=json";
-    var url = "https://play.dhis2.org/demo/api/organisationUnits.json";
+function getData() {
+    var DHIS2Url = "https://play.dhis2.org/demo/api/organisationUnits.json";
     $('#results').append("<h4>Results</h4>");
-    /*
-     $.get(url)
-     .success(someData)
-     .error(console.error);
-     */
-    /*
-     $.getJSON(url, function(json) {
-     $('#results').append(json);
-     console.log(json);
-     //alert(json);
-     });
-     */
 
     var auth = btoa('admin:district');
     $.ajax({
         type: 'GET',
-        headers: {"Authorization": "Basic "+auth},
-        // params: { j_username: "admin", j_password: "district" },
-        url: 'https://play.dhis2.org/demo/api/organisationUnits.json',
-        success   : function(data) { showData(data);},
-        error: function(xhr) {console.log(xhr)}
+        headers: {"Authorization": "Basic " + auth},
+        url: DHIS2Url,
+        success: function (data) {
+            showData(data);
+        },
+        error: function (xhr) {
+            console.log(xhr)
+        }
     });
-
 }
 
 function showData(data) {
-    $('#results').append("<p>" + JSON.stringify(data, null, 2)+"</p>");
+    $('#results').append("<p>" + JSON.stringify(data, null, 2) + "</p>");
 }
